ae8028cbb738059a5b335d3e4623a0feb37ebcc3
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2020-03-11 11:19:23 +0000

    Adding public init to series

--- Sources/GoodReadsKit/Metadata/series.swift
+++ Sources/GoodReadsKit/Metadata/series.swift
@@ -11,6 +11,11 @@ public struct Series: Codable, Equatable, Hashable {
     public let title: String
     public let entry: Double
     
+    public init(title: String, entry: Double) {
+        self.title = title
+        self.entry = entry
+    }
+    
     public var displayName: String {
         if entry.truncatingRemainder(dividingBy: 1.0) == 0.0 {
             return "Book \(Int(entry)) of \(title)"
